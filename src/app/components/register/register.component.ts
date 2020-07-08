c06b49212dc676d15dd7f87278acd21c7dc73c2d
Ovidio Jose Arteaga <ovidiojosearteaga@gmail.com>
2020-07-08 12:06:12 -0500

    Agregado propiedad input state a componente register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'register',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
+  @Input() currentState:string;
 
   constructor() { }
 
